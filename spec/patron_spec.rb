5aec7a9340e828f0d22b32463c595aeea054126b
Thomas Musselman <tmusselman@gmail.com>
2022-07-27 09:12:40 -0600

    feat: create interests variable and add_interest method for patron class

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe Patron do
     expect(patron_1).to be_an(Patron)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     expect(patron_1.name).to eq("Bob")
   end
 
-  xit 'has some money' do 
+  it 'has some money' do 
     expect(patron_1.spending_money).to eq(20)
   end
 
-  xit 'starts with no interests' do
+  it 'starts with no interests' do
     expect(patron_1.interests).to eq([])
   end
 
-  xit 'can get interests' do
+  it 'can get interests' do
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
 
